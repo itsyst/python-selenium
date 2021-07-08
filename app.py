1bb2cb12e7990d43297d7130ac5e3c9ff7548033
Khaled El Hamzi <contact@elhamzi.me>
2021-07-09 01:12:37 +0200

    automate & test website

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ import config
 
 browser = webdriver.Firefox(executable_path=config.fox_path)
 browser.get(config.url)
+
+sign_in_field = browser.find_element_by_link_text("Sign in")
+sign_in_field.click()
+
+username_field = browser.find_element_by_id("login_field")
+username_field.send_keys(config.username)
+
+password_field = browser.find_element_by_id("password")
+password_field.send_keys(config.password)
+password_field.submit()
